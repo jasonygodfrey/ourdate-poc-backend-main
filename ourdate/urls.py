9c4cf74e7c12d96aa622e67d63feffb50a67ca36
Edmond Dantes <58954212+Edmond7450@users.noreply.github.com>
2023-02-12 17:41:47 +0800

    fix

--- ourdate/urls.py
+++ ourdate/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
-if os.uname().sysname == 'Darwin':
+if settings.DEBUG:  # if os.uname().sysname == 'Darwin':
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
